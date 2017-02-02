bcbbe64be5496721ff442a9cf2fe886a95fed489
DESKTOP-1HSMF0J\kenny <edwin.burgos1994@gmail.com>
2017-02-01 22:02:14 -0400

    Button added

--- CodingTest/Form1.cs
+++ CodingTest/Form1.cs
@@ -16,5 +16,12 @@ namespace CodingTest
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string text = textBox1.Text;
+
+            textBox2.Text = text;
+        }
     }
 }
